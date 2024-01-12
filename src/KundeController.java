4e4e58c2c5484039ae37fe02b428c5acbedbae39
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-12 16:22:41 +0100

    Klasse-KundeController initialisiert. Daten aus Datenbank gelesen und Kunden-Objekte erstellt.

--- src/KundeController.java
+++ src/KundeController.java
@@ -1,3 +1,49 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class KundeController {
+
+    public static void selectKunde(){
+
+        // Erstellung einer Connection
+        Connection connection = null;
+
+        try{
+            // Der Connection über die Methode getConnection aus der Klasse MySQl mit der Datenbank verbinden
+            connection = MySQL.getConnection();
+
+            // statement Objekt erstellen, um statements für die Datenbank zu definieren.
+            Statement statement = connection.createStatement();
+
+            // SQL statements definieren ausführen
+            // alle Datensatzzeilen aus der Tabelle kunde auslesen und in rs abspeichern.
+            ResultSet rs = statement.executeQuery("select * from kunde");
+
+            while(rs.next()){
+                int nummer = rs.getInt("nummer");
+                String name = rs.getString("name");
+
+                new Kunde(nummer, name);
+            }
+
+
+        }catch(SQLException ex){
+            ex.printStackTrace();
+
+        }finally{
+            if(connection != null){
+                try{
+                    connection.close();
+                }catch(SQLException ex){
+                    ex.printStackTrace();
+                }
+
+            }
+
+        }
+
+    }
 
 }
--- src/Onlineshop.java
+++ src/Onlineshop.java
@@ -16,7 +16,12 @@ public class Onlineshop {
             System.out.println("\nArtikel: " + artikel.getBezeichnung() + " => " + artikel.getNummer() + "\nHersteller: " + artikel.getHersteller().getName());
         }
 
-    }
+        // Liest die Daten aus der SQL-Tabelle Kunde aus und erstellt für jede Zeile ein Java-Objekt
+        KundeController.selectKunde();
 
+        for(Kunde k : Kunde.kunden.values()){
+            System.out.println("\nKunde: " + k.getName() + " => " + k.getNummer());
+        }
+    }
 
 }

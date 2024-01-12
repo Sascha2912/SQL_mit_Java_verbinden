5a53c9387e3f82d64542c0f5214d612283549d03
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-12 17:47:24 +0100

    Klasse-Adresse initialisiert. Konstruktor + getter und setter für private Eigenschaften erstellt.

--- src/Bestellung.java
+++ src/Bestellung.java
@@ -1,9 +1,45 @@
+import java.util.HashMap;
+
 public class Bestellung {
 
-    private int nummer;
-    private int kunde;
-    private int rechnungsadresse;
-    private int lieferadresse;
+    public static final HashMap<Integer, Bestellung> bestellungen = new HashMap<>();
+
+    private final int nummer;
+    private final Kunde kunde;
+    private final Adresse rechnungsadresse;
+    private final Adresse lieferadresse;
     private String datum;
+
+    Bestellung(int nummer, Kunde kunde, Adresse rechnungsadresse, Adresse lieferadresse, String datum){
+        this.nummer = nummer;
+        this.kunde = kunde;
+        this.rechnungsadresse = rechnungsadresse;
+        this.lieferadresse = lieferadresse;
+        this.datum = datum;
+
+        bestellungen.put(nummer, this);
+    }
+
+    public int getNummer(){
+        return this.nummer;
+    }
+
+    public Kunde getKunde(){
+        return this.kunde;
+    }
+
+    public Adresse getRechnungsadresse(){
+        return this.rechnungsadresse;
+    }
+    public Adresse getLieferadresse(){
+        return this.lieferadresse;
+    }
+
+    public String getDatum(){
+        return this.datum;
+    }
+    public void setDatum(String datum){
+        this.datum = datum;
+    }
 
 }

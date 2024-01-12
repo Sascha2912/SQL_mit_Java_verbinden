0de553c336ba7f140ce3f16f0c608436774e571e
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-12 15:06:12 +0100

    Klasse-Kunde initialisiert mit Konstruktor + getter und setter für die privaten Eigenschaften

--- src/Kunde.java
+++ src/Kunde.java
@@ -1,6 +1,31 @@
+import java.util.HashMap;
+
 public class Kunde {
 
-    private int nummer;
+    public static final HashMap<Integer, Kunde> kunden = new HashMap<>();
+
+    private final int nummer;
     private String name;
+
+    public Kunde(int nummer, String name){
+        this.nummer = nummer;
+        this.name = name;
+
+        kunden.put(nummer, this);
+    }
+
+    public int getNummer(){
+        return this.nummer;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+
 
 }

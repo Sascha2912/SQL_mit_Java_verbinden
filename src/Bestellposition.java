4a3fec5d1ecd33ce46fcfd741478aafec7fdd75d
Sascha Holzigel <S.Holzigel@gmx.de>
2024-01-12 18:30:59 +0100

    Klasse-Bestellposition initialisiert. Konstruktor + getter und setter für private Eigenschaften erstellt.

--- src/Bestellposition.java
+++ src/Bestellposition.java
@@ -1,7 +1,34 @@
+import java.util.HashMap;
+
 public class Bestellposition {
 
-    private int bestellung;
-    private int artikel;
+    public static final HashMap<Integer, Bestellposition> bestellpositionen = new HashMap<>();
+
+    private final Bestellung bestellung;
+    private final Artikel artikel;
     private int anzahl;
 
+    Bestellposition(Bestellung bestellung, Artikel artikel, int anzahl){
+
+        this.bestellung = bestellung;
+        this.artikel = artikel;
+        this.anzahl = anzahl;
+
+        bestellpositionen.put(bestellung.getNummer() + artikel.getNummer(), this);
+
+    }
+    public Bestellung getBestellung(){
+        return this.bestellung;
+    }
+    public Artikel getArtikel(){
+        return this.artikel;
+    }
+
+    public int getAnzahl(){
+        return this.anzahl;
+    }
+
+    public void setAnzahl(int anzahl){
+        this.anzahl = anzahl;
+    }
 }
